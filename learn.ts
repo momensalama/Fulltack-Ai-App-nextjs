455f0ec958903322e8bc2c77950b48feb15b4a37
momen <momen@momens-MacBook-Air.local>
2024-01-16 10:50:55 +0200

    delete learn.ts file

--- learn.ts
+++
@@ -1,8 +0,0 @@
-type Fn = (a: number, b: string) => number;
-
-type Result = AppendArgument<Fn, boolean>;
-// expected be (a: number, b: string, x: boolean) => number
-
-type AppendArgument<Fn, T> = Fn extends (...arg: infer R) => infer A
-  ? (...arg: [...R, T]) => A
-  : never;
